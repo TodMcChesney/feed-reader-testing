89acb422517f02a8bc476d4301cb126cda7077c4
TodMcChesney <codescribe.net@gmail.com>
2017-06-29 11:25:05 +0200

    style: Reformat test comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,27 +9,15 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    /* Test suite named 'RSS Feeds' */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        // Test allFeeds to ensure it is defined and that it's not empty
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* This test loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        // Test each feed to ensure it has a URL defined and it's not empty
         it('have URLs', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.url).toBeDefined();
@@ -37,10 +25,7 @@ $(function() {
             });
         });
 
-        /* This test loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        // Test each feed to ensure it has a name defined and it's not empty
         it('have names', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.name).toBeDefined();
@@ -54,18 +39,12 @@ $(function() {
         var body = $('body');
         var menuIcon = $('.menu-icon-link');
 
-        /* This test ensures the menu element is hidden by default.
-         * It looks to see if the body element has the menu-hidden class
-         * when the page loads.
-         */
+        // Test to ensure the menu element is hidden by default
         it('is hidden by default', function() {
             expect(body.hasClass('menu-hidden')).toBe(true);
         });
 
-        /* This test ensures the menu changes visibility when the
-         * menu icon is clicked. The first click makes it visible and
-         * the second click hides it.
-         */
+        // Test the menu show/hide toggle when icon is clicked
         it('changes visibility when the icon is clicked', function() {
             menuIcon.click();
             expect(body.hasClass('menu-hidden')).toBe(false);
